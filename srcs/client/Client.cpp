53326937e8a24d397f056176ead3cdc0a54984e2
Jinho Heo <uj07077@gmail.com>
2023-11-05 19:15:23 +0900

    fix: email parsing

--- srcs/client/Client.cpp
+++ srcs/client/Client.cpp
@@ -14,7 +14,6 @@ void Client::Login() {
 
   std::cout << "아이디를 입력하세요: ";
   std::cin >> input_id;
-  // clearerr(stdin);
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n'); // 버퍼를 비움
 
   std::cout << "비밀번호를 입력하세요: ";
@@ -35,13 +34,8 @@ void Client::Login() {
   strcpy(authplain + 1, input_id.c_str());
   strcpy(authplain + 1 + input_id.size() + 1, input_password.c_str());
   
-  // std::string authplain;
-  // authplain = "\0" + input_id + "\0" + input_password;
-  // std::cout<< "auth plain: " << authplain <<std::endl;
   std::string authplain_encoded = base64_encode(reinterpret_cast<const unsigned char*>(authplain), input_id.size() + input_password.size() + 2);
-  // std::string authplain_encoded = base64_encode(reinterpret_cast<const unsigned char*>(authplain.data()), authplain.size());
-  //std::cout << "auth plain encoded: " << authplain_encoded << std::endl;
-
+  
   if (mailbox) {
     delete mailbox;
     mailbox = nullptr;
@@ -49,10 +43,6 @@ void Client::Login() {
 
   mailbox = new MailBox(input_id, input_password, authplain_encoded);
   mailbox->pop3.ConnectPOP3();
-
-  //std::memset(&input_id[0], 0, input_id.size());
-  //std::memset(&input_password[0], 0, input_password.size());
-  //std::memset(&authplain[0], 0, authplain.size());
 
   input_id.clear();
   input_password.clear();
@@ -66,7 +56,6 @@ void Client::Logout() {
     delete mailbox;
   }
   mailbox = nullptr;
-  // std::cout << "로그아웃 완료!";
 }
 
 Email Client::EmailInput() {
@@ -74,7 +63,6 @@ Email Client::EmailInput() {
 
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n'); // 버퍼를 비움
   std::cout << "받는 사람의 이메일 주소를 입력하세요: ";
-  // std::cin>>sendTo;
   std::getline(std::cin, sendTo);
   std::cout << "제목을 입력하세요: ";
   std::getline(std::cin, title);
@@ -107,11 +95,9 @@ void Client::ShowOptions() {
     std::cout << "1. 이메일 발송\n";
     std::cout << "2. 이메일 수신 (리스트 보기)\n";
     std::cout << "3. 특정 이메일 개별 접근 (인덱스 번호 입력)\n";
-    std::cout << "4. 이메일 전달\n";
-    std::cout << "5. 이메일 답장\n";
-    std::cout << "6. 이메일 삭제\n";
-    std::cout << "7. 로그아웃\n";
-    std::cout << "8. 종료\n";
+    std::cout << "4. 이메일 답장\n";
+    std::cout << "5. 이메일 삭제\n";
+    std::cout << "6. 로그아웃 및 종료\n";
   }
   int option;
 
@@ -124,7 +110,6 @@ void Client::ShowOptions() {
     std::cout << "번호 입력 :";
     std::cin >> option;
   }
-  // std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   switch (option) {
   case 1:
     // 이메일 발송
@@ -157,23 +142,6 @@ void Client::ShowOptions() {
       break;
     }
   case 4:
-    // 이메일 전달
-    {
-      int id;
-      std::cout << "전달할 이메일 id 입력 : ";
-      std::string sendTo;
-      std::cin >> id >> sendTo;
-      while (std::cin.fail()) {
-        std::cin.clear(); // 오류 상태 초기화
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n'); // 오류 입력 제거
-        std::cout << "잘못된 입력입니다. 다시 시도해주세요.\n" << std::endl;
-        std::cout << "전달할 이메일 id 입력 : ";
-        std::cin >> id >> sendTo;
-      }
-      mailbox->ForwardMail(id, sendTo);
-      break;
-    }
-  case 5:
     // 이메일 답장
     {
       int id;
@@ -198,7 +166,7 @@ void Client::ShowOptions() {
       mailbox->ReplyMail(id, body);
       break;
     }
-  case 6:
+  case 5:
     // 이메일 삭제
     {
       int id;
@@ -214,11 +182,7 @@ void Client::ShowOptions() {
       mailbox->pop3.DeleteMessage(id);
       break;
     }
-  case 7:
-    // 로그아웃
-    Logout();
-    break;
-  case 8:
+  case 6:
     // 종료
     Logout();
     std::cout << "종료합니다.\n";
--- srcs/core/Email.cpp
+++ srcs/core/Email.cpp
@@ -23,20 +23,41 @@ Email::Email(std::list<std::string> &rawEmail) {
   for (const auto &line : rawEmail) {
 		std::cout << "line: " << line << std::endl;
     if (line.substr(0, 5) == "From:") {
-      m_nickname = decodeMimeWord(line.substr(6));
-      size_t start = line.find('<');
-      size_t end = line.find('>');
 
-      if (start != std::string::npos && end != std::string::npos &&
-          start < end) {
-        m_recvFrom += line.substr(start + 1, end - start - 1);
+      if (line[5] == ' ') {
+        m_nickname = decodeMimeWord(line.substr(6));
+        size_t start = line.find('<');
+        size_t end = line.find('>');
+        if (start != std::string::npos && end != std::string::npos &&
+            start < end) {
+          m_recvFrom = line.substr(start + 1, end - start - 1);
+        } else{
+          m_recvFrom = line.substr(6);
+        }
+      } else {
+        m_recvFrom = line.substr(5);
       }
     } else if (line.substr(0, 3) == "To:") {
-      m_sendTo = line.substr(4);
+      if (line[3] == ' ') {
+        m_sendTo = line.substr(4);
+      } else {
+        m_sendTo = line.substr(3);
+      }
+      // m_sendTo = line.substr(4);
     } else if (line.substr(0, 5) == "Date:") {
-      m_date = line.substr(6);
+      if (line[5] == ' ') {
+        m_date = line.substr(6);
+      } else {
+        m_date = line.substr(5);
+      }
+      // m_date = line.substr(6);
     } else if (line.substr(0, 8) == "Subject:") {
-      m_title = decodeMimeWord(line.substr(9));
+      if (line[8] == ' ') {
+        m_title = decodeMimeWord(line.substr(9));
+      } else {
+        m_title = decodeMimeWord(line.substr(8));
+      }
+      // m_title = decodeMimeWord(line.substr(9));
     } else if (line.substr(0, 27) == "Content-Transfer-Encoding:") {
       isBase64Content = line.find("base64") != std::string::npos;
     } else if (isBase64Content) {
